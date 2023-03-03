d95176f6b078d719ae02b8bff9fc0c6347cb4a5e
Sera lee <ssellu.lee@gmail.com>
2023-03-03 15:49:27 +0900

    Update .gitignore

--- modules/image_module/include/image_module/extract_features.hpp
+++ modules/image_module/include/image_module/extract_features.hpp
@@ -6,65 +6,19 @@
 #include <time.h>
 #include <string>
 
-class Extractor{
+class ImageProcessor{
 public:
-    Extractor();
-    ~Extractor();
-    virtual void detectAndCompute(const cv::Mat& image_bgr, cv::Mat& descriptor);
-    virtual void drawKeypoints(const cv::Mat& image_bgr, const std::vector<cv::KeyPoint>& keypoints);
-protected:
-    cv::Ptr<cv::Feature2D> feature;
+    ImageProcessor(const std::string feature_type);
+    ~ImageProcessor();
+    // void detectAndCompute(const cv::Mat& image_bgr);
+
+    void detectAndCompute(const cv::Mat& image_bgr, cv::Mat& descriptor);
+
+private:
+    std::string feature_type_;
     bool draw;
+    cv::Ptr<cv::Feature2D> feature;
 };
 
-class ORB : public Extractor{
-public:
-    ORB();
-    ORB(const int nFeatures);
-    ~ORB();
-    void detectAndCompute(const cv::Mat& image_bgr, cv::Mat& descriptor);
-    void drawKeypoints(const cv::Mat& image_bgr, const std::vector<cv::KeyPoint>& keypoints);
-private:
-};
-
-class AKAZE : public Extractor{
-public:
-    AKAZE();
-    AKAZE(const int nFeatures);
-    ~AKAZE();
-    void detectAndCompute(const cv::Mat& image_bgr, cv::Mat& descriptor);
-    void drawKeypoints(const cv::Mat& image_bgr, const std::vector<cv::KeyPoint>& keypoints);
-private:
-};
-
-class KAZE : public Extractor{
-public:
-    KAZE();
-    KAZE(const int nFeatures);
-    ~KAZE();
-    void detectAndCompute(const cv::Mat& image_bgr, cv::Mat& descriptor);
-    void drawKeypoints(const cv::Mat& image_bgr, const std::vector<cv::KeyPoint>& keypoints);
-private:
-};
-
-class SIFT : public Extractor{
-public:
-    SIFT();
-    SIFT(const int nFeatures);
-    ~SIFT();
-    void detectAndCompute(const cv::Mat& image_bgr, cv::Mat& descriptor);
-    void drawKeypoints(const cv::Mat& image_bgr, const std::vector<cv::KeyPoint>& keypoints);
-private:
-};
-
-class BRISK : public Extractor{
-public:
-    BRISK();
-    BRISK(const int nFeatures);
-    ~BRISK();
-    void detectAndCompute(const cv::Mat& image_bgr, cv::Mat& descriptor);
-    void drawKeypoints(const cv::Mat& image_bgr, const std::vector<cv::KeyPoint>& keypoints);
-private:
-};
 
 #endif //EXTRACT_FEATURES_HPP
